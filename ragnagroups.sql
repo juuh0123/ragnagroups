bedfcbde7c38929aa3c91bc2836b0d23c0298644
Daniel Vieira Junior <danieljunior19942009@hotmail.com>
2016-02-02 16:49:42 -0200

    Alteracoes no db, conceito, estrutura.

--- ragnagroups.sql
+++ ragnagroups.sql
@@ -4,37 +4,46 @@ DROP DATABASE IF EXISTS ragnagroups;
 CREATE DATABASE  ragnagroups;
 USE ragnagroups;
 
-CREATE TABLE IF NOT EXISTS `usuarios`(
-	id				INT AUTO_INCREMENT PRIMARY KEY,
-	login			VARCHAR(50) UNIQUE NOT NULL,
-	nome		VARCHAR(50) NOT NULL,
-	senha		VARCHAR(50) NOT NULL,
-	email 		VARCHAR(80) UNIQUE NOT NULL,
-	ativo   		CHAR(2), /*s ou n para ativo ou inativo*/
-	dataCad	DATE NOT NULL,
-	foto			VARCHAR(200), /*foto de perfil*/
-	post			VARCHAR(500), /*esse campo é para pegar os id's dos post do usuario*/ 
-	topico 		VARCHAR(100), /*esse campo deve receber os id's dos tópico do usuário, apenas topicos criados por ele.*/	
-	ip				VARCHAR(20) /*guardar os ip's do usuário*/
+CREATE TABLE IF NOT EXISTS `users`(
+	user_id						INT AUTO_INCREMENT PRIMARY KEY,
+	user_login					VARCHAR(50) UNIQUE NOT NULL,
+	user_name				VARCHAR(50) NOT NULL,
+	user_pass					VARCHAR(50) NOT NULL,
+	user_mail 					VARCHAR(80) UNIQUE NOT NULL,
+	user_ativo   				CHAR(2), /*s ou n para ativo ou inativo*/
+	user_date					DATE NOT NULL,/*data de criação da conta*/
+	user_photo				VARCHAR(200), /*foto de perfil*/
+	user_post_id	(ce)		VARCHAR(500), /*esse campo é para pegar os id's dos post do usuario*/ 
+	user_topic_id (ce)		VARCHAR(100), /*esse campo deve receber os id's dos tópico do usuário, apenas topicos criados por ele.*/	
+	user_ip						VARCHAR(20) /*guardar os ip's do usuário*/
 );
 
-CREATE TABLE IF NOT EXISTS `topico`(
-	topID				INT AUTO_INCREMENT PRIMARY KEY,
-	topAutor			VARCHAR(50) NOT NULL, /*nome do autor do topico ou login*/
-	topNome			VARCHAR(100) NOT NULL UNIQUE, /*o nome do topico deve ser unico, caso tenha algo relacionado o mesmo deve ser um post dentro do topico */
-	topPost				VARCHAR NOT NULL, /*esse campo deve conter todos posts, perguntas, respostas relacionados aos topicos, onde deve pegar o login/id do usario que comentar*/
-	topConteudo 		VARCHAR NOT NULL, /*esse campo deve pegar todo conteudo do topico, ou seja, listar todos pots, caso queira pegar os posts daquele topico uso o campo topPost*/
-	topDate				TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00' /*data de criação do topico*/
+CREATE TABLE IF NOT EXISTS `topics`(
+	top_id					INT AUTO_INCREMENT PRIMARY KEY,
+	top_user_id (ce)	VARCHAR(50) NOT NULL, /*nome do autor do topico ou login*/
+	top_name				VARCHAR(100) NOT NULL UNIQUE, /*o nome do topico deve ser unico, caso tenha algo relacionado o mesmo deve ser um post dentro do topico */
+	top_post				VARCHAR NOT NULL, /*esse campo deve conter todos posts, perguntas, respostas relacionados aos topicos, onde deve pegar o login/id do usario que comentar*/
+	top_container		VARCHAR NOT NULL, /*esse campo deve pegar todo conteudo do topico, ou seja, listar todos pots, caso queira pegar os posts daquele topico uso o campo topPost*/
+	top_date				TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00' /*data de criação do topico*/
 );
 
 CREATE TABLE IF NOT EXISTS `posts`( /*cada post deve estar atrelado à um tópico.*/
-	postID				INT AUTO_INCREMENT PRIMARY KEY,
-	postAutor			VARCHAR(50) NOT NULL UNIQUE, /*esse campo deve receber o login ou id do usuario autor do post*/
-	postNome			VARCHAR(100) UNIQUE NOT NULL, /*nome do tópico ou post*/
-	postConteudo	VARCHAR(500) NOT NULL, /*ess campo recebe todo conteudo do post, devo converter com php todos cararetes para htmml special chars...*/
-	postData			TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00' /*data de criação do post*/
+	post_id					INT AUTO_INCREMENT PRIMARY KEY,
+	post_user_id	(ce)	VARCHAR(50) NOT NULL UNIQUE, /*esse campo deve receber o login ou id do usuario autor do post*/
+	post_name			VARCHAR(100) UNIQUE NOT NULL, /*nome do post*/
+	post_container		VARCHAR(500) NOT NULL, /*ess campo recebe todo conteudo do post, ouse ja, os comentarios, devo converter com php todos cararetes para htmml special chars...*/
+	/*Este post_container pode ser um atributo multivalorado ou uma nova entidade, ex: postConteudo possui: coments(a string contendo o comentario todo), date coments(data do comentario), user_coments(o id do usuario q comentou)*/
+	post_date				TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00', /*data de criação do post*/
+	post_top_id	(ce)	INT /*Este campo é responsável por pegar o id do topico ao qual ele pertence, ou seja, ou post sempre esta dentro de um topico*/ 
 );
 
+CREATE TABLE IF NOT EXISTS `coments`(/*todo comentario esta contido em um post e a um user, talvez, preciso ver...se há um relaciomento tbm entre os comentarios e os topicos*/
+	com_id					INT AUTO_INCREMENT PRIMARY KEY,
+	com_user_id (ce) 	INT NOT NULL, /*chave estrangeira, identifica o usuário que comentou, quem comentou*/
+	com_post_id (ce) 	INT NOT NULL, /*Chave estrangeira, a qual post pertence/esta atrelado*/
+	com_container 		VARCHAR(500) NOT NULL,  /*Contem a string do comentario, ou seja, o input textArea que o usuario submeter eu vou jogar aqui depois de validar seu conteudo*/
+	com_date				TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00'/*quando foi comentado*/
+);
 INSERT INTO usuarios(id, login, nome, senha, email, ativo, dataCad, foto, post, topico, ip) VALUES(1, 'admin', 'daniel', 'admin', 'djr@br.ibm.com', 's', '10-01-2016',  'null', 'null', 'null', 'null');
 
 
